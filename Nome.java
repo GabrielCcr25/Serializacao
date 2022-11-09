ca3b4cc1fbe721540a2f0102a123ce3425c947cc
Gabriel Lima <108353430+GabrielCcr25@users.noreply.github.com>
2022-11-08 21:52:37 -0300

    Add files via upload

---
+++ Nome.java
@@ -0,0 +1,22 @@
+package br.com.ExemploIO;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Nome {
+    public static void leitor(String path) throws IOException{
+        int i;
+         ArrayList<String> nomes = new ArrayList<>();
+        BufferedReader buffReader = new BufferedReader(new FileReader(path));
+        for(i=0;i<10;i++){
+            
+
+
+        }
+
+    }
+
+}
---
+++ Sobrenome.java
@@ -0,0 +1,4 @@
+package br.com.ExemploIO;
+
+public class Sobrenome {
+}
